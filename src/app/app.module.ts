066abcf42c122204530c2261aacf9ac8d5f7a036
tristan.wittgen <tristanwittgen@gmail.com>
2025-04-23 23:53:04 +0200

    some cleanup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,5 @@
 import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ComponentsModule } from './components/components.module';
 import { FormsModule } from '@angular/forms';
@@ -17,7 +15,6 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     ComponentsModule,
     FormsModule,
     HttpClientModule,
