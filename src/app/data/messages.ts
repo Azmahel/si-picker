a9998348e066f4e7fc8336084f181932cabba6a4
tristan.wittgen <tristanwittgen@gmail.com>
2025-04-23 18:53:45 +0200

    add apocrypha message

--- src/app/data/messages.ts
+++ src/app/data/messages.ts
@@ -5,12 +5,13 @@ export interface Messages {
 }
 
 export const messages: Messages = {
-    expansionNames: { 
+    expansionNames: {
         [Expansion.BASE] : "Spirit Island",
         [Expansion.BRANCH_CLAW] : "Branch and Claw",
         [Expansion.JAGGED_EARTH] : "Jagged Earth" ,
         [Expansion.FEATHER_FLAME] : "Feather and Flame",
         [Expansion.HORIZONS] : "Horizons of Spirit Island" ,
         [Expansion.NATURE_INCARNATE] : "Nature Incarnate" ,
+        [Expansion.APOCRYPHA] : "Apocrypha" ,
     }
 }
